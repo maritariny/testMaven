d01d21a0b483629d42ce7386c3b34af4bd6637b0
Marina Malygina <marina0391@mail.ru>
2022-03-29 00:36:13 +0300

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+import org.apache.commons.lang3.StringUtils;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        StringUtils.capitalize("hello");
+    }
+}
